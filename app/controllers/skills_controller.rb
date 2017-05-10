929348807ea6267e3935da8ef3dd2f34e9d4e309
Jody Lipkin <lipkinjody@gmail.com>
2017-05-09 20:44:30 -0700

    backend api fixes

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,15 @@
 class SkillsController < ApplicationController
+
+	def index 
+		@id = 1
+		@student = Unirest.get("https://sheltered-chamber-15774.herokuapp.com/api/v2/students/#{@id}.json").body
+		@skills = @student["skills"]
+	end
+
+	def edit
+		@id = 1
+		@student = Unirest.get("https://sheltered-chamber-15774.herokuapp.com/api/v2/students/#{@id}.json").body
+		@skills = @student["skills"]
+	end
+
 end
